de949709b39259c8016b4709794d6b6f95c7a555
MOZTECH-Main <250109731+MOZTECH-Main@users.noreply.github.com>
2026-07-21 14:59:45 +0800

    Deploy account and co-creation preview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1992579565" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1216834436" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
