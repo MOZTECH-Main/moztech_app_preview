27fd7addd5b37a71f4d164ac3a726646a41b8242
MOZTECH-Main <250109731+MOZTECH-Main@users.noreply.github.com>
2026-07-27 07:05:30 +0800

    Deploy interactive voice capture preview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2769164007" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "650182953" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
