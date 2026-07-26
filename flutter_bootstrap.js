dabfb11663f2eb991b57b64d9a0ae5bb65045609
MOZTECH-Main <250109731+MOZTECH-Main@users.noreply.github.com>
2026-07-26 14:03:21 +0800

    Deploy flatter service journeys

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1693703825" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1185935795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
