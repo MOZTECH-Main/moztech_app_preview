c0dc0d1c844af61268de9b867ebac04e6fd32c44
MOZTECH-Main <250109731+MOZTECH-Main@users.noreply.github.com>
2026-07-27 19:40:23 +0800

    Fix GitHub Pages base path

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "275670401" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3808080392" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
